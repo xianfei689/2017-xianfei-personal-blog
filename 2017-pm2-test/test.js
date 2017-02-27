9c20879a926bed89cd62b03a6f6a4d7c0ffc69ba
xianfei689 <347560189@qq.com>
2017-02-27 15:07:38 +0800

    22

--- 2017-pm2-test/test.js
+++ 2017-pm2-test/test.js
@@ -1,11 +1,15 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
+http.createServer(function (request, response) {
 
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+    // 发送 HTTP 头部
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
 
-  res.end('Hello World\n');
+    // 发送响应数据 "Hello World"
+    response.end('Hello World\n');
+}).listen(80);
 
-}).listen(80,"0.0.0.0");
-
-console.log('Server running.');
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:80/');
